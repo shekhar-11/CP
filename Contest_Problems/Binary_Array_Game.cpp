cd0acd67b929a82d8e0c4d840e3b5ef04ad8d318
shekhar-11 <rajshekhar111201@gmail.com>
2026-01-08 00:59:31 +0530

    Upsolve

---
+++ Contest_Problems/Binary_Array_Game.cpp
@@ -0,0 +1,71 @@
+//https://codeforces.com/contest/2183/problem/A
+
+
+//MISTAKE -----  READ THE PROBLEM PROPERLY --------------- HAD TO BRING THE ENTIRE ARRAY TO SIZE 1 but I focused on making all elements identical (for 0 at start and  end)
+
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+
+
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+
+
+#define umii unordered_map<int,int>
+#define mii  map<int,int>
+#define umib unordered_map<int,bool>
+#define mib  map<int,bool>
+#define umll unordered_map<ll,ll>
+#define mll  map<ll,ll>
+#define ulb unodered_map<ll,bool>
+
+#define umci unordered_map<char,int>
+#define umcb unordered_map<char,bool>
+#define mci map<char,int>
+#define mcb map<char,bool>
+
+#define ip(x) for(auto &it:x)cin>>it
+#define op(x) for(auto &it:x) cout<<it<<" "
+
+
+
+
+
+void solve()
+{
+	ll n;
+	cin>>n;
+	vll v(n,0);
+	ip(v);
+    
+   if(v[0]==0 && v[n-1]==0)
+   cout<<"Bob\n";
+   else
+    cout<<"Alice\n";
+   
+
+
+}
+
+
+
+int main()
+{
+	int t=1;
+	cin>>t;
+	while(t--)solve();
+	return 0;
+}
+
+
